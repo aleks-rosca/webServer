c59158cc7d09d1f8949f9dc17104b911f1ed04f6
aleks-rosca <Trunks3223>
2020-04-30 10:23:16 +0200

    order

--- webServer/src/main/java/com/restaurant/webServer/dao/orderDB.java
+++ webServer/src/main/java/com/restaurant/webServer/dao/orderDB.java
@@ -1,4 +1,30 @@
 package com.restaurant.webServer.dao;
 
-public class orderDB {
+import com.restaurant.webServer.data.Conn;
+import com.restaurant.webServer.model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
+
+@Repository("postgres")
+public class orderDB implements orderDAO {
+
+    private Conn connection;
+
+    public orderDB() {
+        connection = Conn.getInstance();
+    }
+
+    @Override
+    public String addOrder(Order order) {
+        String sql = "INSERT INTO item values('" + order.getOrderID() + "', '" + order.getListID() + "', '" + order.getTotalPrice() + "', '" + order.getTableNo() + "', CURRENT_DATE);";
+
+        try {
+            connection.update(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
+
